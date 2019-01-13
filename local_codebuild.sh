2983fee6642208fa5ab4d032a78d13bcc2da0e97
Marvin Lukas Wenuel <marvinl.wenzel@googlemail.com>
2019-01-13 15:02:02 +0100

    re-rooting

--- local_codebuild.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-ARTIFACTS_FOLDER=/home/marvin/dev/systemintegration/raq/raqcrawl/artifacts
-rm -rf ${ARTIFACTS_FOLDER}
-pyb clean
-./codebuild_build.sh -i python:3.6-stretch -e local.env -a $ARTIFACTS_FOLDER
-unzip -l ${ARTIFACTS_FOLDER}/artifacts.zip
