4130ecf58d6f749ac511e1fd2fa86ae5891bac90
Marvin Lukas Wenuel <marvinl.wenzel@googlemail.com>
2019-01-22 22:39:39 +0100

    Little refactoring

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -3,8 +3,8 @@ import datetime
 import os
 import random
 import string
-import subprocess
 import sys
+from subprocess import PIPE, run
 
 import boto3
 
@@ -26,7 +26,7 @@ def read_config_from_environment(stage: str = "DEV"):
 
 def run_crawler_with_config(config):
     """Instantiates connections and instances, ties them together and runs the crawler."""
-    global STATS
+    global GLOBAL
 
     github_session = GithubSession()
     github_session.set_credentials(personal_access_token=config['github_token'])
@@ -40,16 +40,15 @@ def run_crawler_with_config(config):
                                 queue_address=config['msg_queue_address'],
                                 msg_visibility_timeout=30)
 
-    my_prefix = 'raq_crawler_{}'.format(STATS['RANDOM_ID'])
+    my_prefix = 'raq_crawler_{}'.format(GLOBAL['RANDOM_ID'])
     # working_temp_dir = tempfile.TemporaryDirectory()
     # working_path = working_temp_dir.name
 
     os.makedirs('/tmp/{}'.format(my_prefix))
     working_path = '/tmp/{}'.format(my_prefix)
-    should_run = True
-    STATS['ORIGIN_DIR'] = working_path
-    while should_run:
-        os.chdir(STATS['ORIGIN_DIR'])
+    GLOBAL['ORIGIN_DIR'] = working_path
+    while GLOBAL['SHOULD_RUN']:
+        os.chdir(GLOBAL['ORIGIN_DIR'])
         message = msg_queue.pop_next_message()
         if github_session.rate is not None:
             print('Current rate left: {}'.format(github_session.rate))
@@ -60,106 +59,121 @@ def run_crawler_with_config(config):
         if 'task_type' not in message.body_dict:
             print(message.body_dict)
             print('bye bye')
-            should_run = False
+            GLOBAL['SHOULD_RUN'] = False
         if message.body_dict['task_type'] == 'repo':
-            try:
-                print('Received repo_task')
-                print(message.body_dict)
-                print(message.message_attributes)
-
-                task = message.body_dict['repo_task']
-
-                repo_path = working_path + "/{}".format(task['id'])
-                os.makedirs(repo_path)
-            except FileExistsError as e:
-                print(e)
-            else:
-                res_dict, headers, resp_body = github_session.request_url(task['api_url'])
-                metaf_name = repo_path + "/meta.json"
-
-                print('Metaf {}'.format(metaf_name))
-
-                metaf = open(metaf_name, 'w')
-                metaf.write(resp_body)
-                metaf.flush()
-                metaf.close()
-
-                try:
-                    repo_git_path = repo_path + "/git_repo/"
-                    print("Cloning {}, {}".format(res_dict['id'], res_dict['full_name']))
-                    pop = subprocess.run(['git', 'clone', res_dict['clone_url'], repo_git_path], universal_newlines=True)
-                    pop.check_returncode()
-                except Exception as e:
-                    print('Aborting {}, {} because of error'.format(res_dict['id'], res_dict['full_name']))
-                    print(e)
-                else:
-                    os.chdir(repo_git_path)
-                    log_process = subprocess.run(['git', '--no-pager', 'log', '--pretty=%H', '--max-count=1'], stdout=subprocess.PIPE, encoding='utf-8', universal_newlines=True)
-                    initial_sha = log_process.stdout
-                    if initial_sha[-1] == '\n':
-                        initial_sha = initial_sha[:-1]
-                    agenda = [initial_sha]
-                    done_shas = []
-                    for current_sha in agenda:
-                        if current_sha in done_shas:
-                            continue
-                        if current_sha == '':
-                            continue
-                        delimiter = '###wtf###'
-                        parents_format = '--pretty=format:%P{}'.format(delimiter)
-                        combi_format = '--pretty=%H %T %P %N{}'.format(delimiter)
-                        show_commit_cmd = ['git', '--no-pager', 'show', '--shortstat', combi_format, current_sha]
-                        parents_cmd = ['git', '--no-pager', 'show', '--shortstat', parents_format, current_sha]
-
-                        try:
-                            parents_proc = subprocess.run(parents_cmd, stdout=subprocess.PIPE, encoding='unicode-escape', universal_newlines=True, stderr=subprocess.PIPE)
-                            parents_exit_code = parents_proc.returncode
-                            parents_out = parents_proc.stdout
-
-                            if parents_exit_code != 0:
-                                pass
-                            else:
-                                if (delimiter + "\n") not in parents_out:
-                                    pass
-                                parent_shas = parents_out.split(delimiter)[0].split(" ")
-                                if len(parent_shas) == 1 and parent_shas[0] == '':
-                                    pass
-                                agenda.extend(parent_shas)
-
-                            show_commit_proc = subprocess.run(show_commit_cmd, stdout=subprocess.PIPE, encoding='unicode-escape')
-
-                            show_commit_exit_code = show_commit_proc.returncode
-                            show_out = show_commit_proc.stdout
-
-                            if show_commit_exit_code != 0:
-                                pass
-                            else:
-                                x = show_out.split(delimiter)
-                                myout = x[0]
-                                leftover = ''.join(x[1:])
-                                shaf_name = repo_path + "/{}.json".format(current_sha)
-
-                                print('shaf {}'.format(shaf_name))
-
-                                shaf = open(shaf_name, 'w')
-                                shaf.write(myout)
-                                shaf.flush()
-                                shaf.close()
-                        except Exception as e:
-                            print(e)
-                            print(e)
-                        done_shas.append(current_sha)
-            print('Done')
+            handle_repo_task(github_session, message, working_path)
         elif message.body_dict['task_type'] == 'refill':
             handle_refill_task(github_session, message, msg_queue)
         elif message.body_dict['task_type'] == 'kill-15':
-            should_run = False
+            GLOBAL['SHOULD_RUN'] = False
             msg_queue.write_message(message_dict={'task_type': 'stop'})
         message.delete()
 
 
+def handle_repo_task(github_session, message, working_path):
+    try:
+        task = message.body_dict['repo_task']
+        print('Received repo_task')
+        print(message.body_dict)
+        print(message.message_attributes)
+        repo_task_path = working_path + "/{}".format(task['id'])
+        os.makedirs(repo_task_path)
+    except FileExistsError as e:
+        print(e)
+        return 0x10
+
+    repo_meta_dict, headers, resp_body = github_session.request_url(task['api_url'])
+    meta_json_full_file_name = repo_task_path + "/meta.json"
+    print('Meta.json {}'.format(meta_json_full_file_name))
+    write_meta_json(meta_json_full_file_name, resp_body)
+
+    try:
+        repo_git_path = repo_task_path + "/git_repo/"
+        print("Cloning {} {}".format(repo_meta_dict['id'], repo_meta_dict['full_name']))
+        clone_repository_into_directory(target_path=repo_git_path, clone_url=repo_meta_dict)
+    except Exception as e:
+        print('Aborting {}, {} because of error'.format(repo_meta_dict['id'], repo_meta_dict['full_name']))
+        print(e)
+        return
+
+    os.chdir(repo_git_path)
+    log_process = run(['git', '--no-pager', 'log', '--pretty=%H', '--max-count=1'],
+                      stdout=PIPE, encoding='utf-8', universal_newlines=True)
+    initial_sha = log_process.stdout
+
+    if initial_sha[-1] == '\n':
+        initial_sha = initial_sha[:-1]
+
+    agenda = [initial_sha]
+    done_shas = []
+    for current_sha in agenda:
+        if current_sha in done_shas:
+            continue
+        if current_sha == '':
+            continue
+
+        delimiter = '###wtf###'
+        parents_format = '--pretty=format:%P{}'.format(delimiter)
+        combi_format = '--pretty=%H %T %P %N{}'.format(delimiter)
+        show_commit_cmd = ['git', '--no-pager', 'show', '--shortstat', combi_format, current_sha]
+        parents_cmd = ['git', '--no-pager', 'show', '--shortstat', parents_format, current_sha]
+
+        try:
+            parents_proc = run(parents_cmd, stdout=PIPE, encoding='unicode-escape',
+                               universal_newlines=True, stderr=PIPE)
+            parents_exit_code = parents_proc.returncode
+            parents_out = parents_proc.stdout
+
+            if parents_exit_code != 0:
+                pass
+            else:
+                if (delimiter + "\n") not in parents_out:
+                    pass
+                parent_shas = parents_out.split(delimiter)[0].split(" ")
+                if len(parent_shas) == 1 and parent_shas[0] == '':
+                    pass
+                agenda.extend(parent_shas)
+
+            show_commit_proc = run(show_commit_cmd, stdout=PIPE,
+                                   encoding='unicode-escape')
+
+            show_commit_exit_code = show_commit_proc.returncode
+            show_out = show_commit_proc.stdout
+
+            if show_commit_exit_code != 0:
+                pass
+            else:
+                x = show_out.split(delimiter)
+                myout = x[0]
+                leftover = ''.join(x[1:])
+                shaf_name = repo_task_path + "/{}.json".format(current_sha)
+
+                print('shaf {}'.format(shaf_name))
+
+                shaf = open(shaf_name, 'w')
+                shaf.write(myout)
+                shaf.flush()
+                shaf.close()
+        except Exception as e:
+            print(e)
+        done_shas.append(current_sha)
+    print('Done')
+
+
+def clone_repository_into_directory(target_path: str, clone_url: str):
+    pop = run(['git', 'clone', clone_url, target_path], universal_newlines=True)
+    pop.check_returncode()
+
+
+def write_meta_json(metaf_name, resp_body):
+    metaf = open(metaf_name, 'w')
+    metaf.write(resp_body)
+    metaf.flush()
+    metaf.close()
+
+
 def handle_refill_task(github_session, message, msg_queue):
-    global STATS
+    global GLOBAL
 
     print('Received refill_task')
     print(message.body_dict)
@@ -179,11 +193,11 @@ def handle_refill_task(github_session, message, msg_queue):
         task_attr = {
             'creator_id': {
                 'DataType': "String",
-                'StringValue': STATS['RANDOM_ID']
+                'StringValue': GLOBAL['RANDOM_ID']
             },
             'creator_started_timestamp': {
                 'DataType': "String",
-                'StringValue': STATS['START_TIMESTAMP']
+                'StringValue': GLOBAL['START_TIMESTAMP']
             },
         }
         last_id = int(repo_dict['id'])
@@ -199,11 +213,11 @@ def handle_refill_task(github_session, message, msg_queue):
     task_attr = {
         'creator_id': {
             'DataType': "String",
-            'StringValue': STATS['RANDOM_ID']
+            'StringValue': GLOBAL['RANDOM_ID']
         },
         'creator_started_timestamp': {
             'DataType': "String",
-            'StringValue': STATS['START_TIMESTAMP']
+            'StringValue': GLOBAL['START_TIMESTAMP']
         },
     }
     print('refill_task adding since id {} '.format(last_id))
@@ -214,12 +228,13 @@ def handle_refill_task(github_session, message, msg_queue):
 
 
 if __name__ == '__main__':
-    STATS = {
+    GLOBAL = {
         'START_TIMESTAMP': datetime.datetime.utcnow().isoformat(),
-        'RANDOM_ID': ''.join(random.choices(string.ascii_letters + string.digits, k=32))
+        'RANDOM_ID': ''.join(random.choices(string.ascii_letters + string.digits, k=32)),
+        'SHOULD_RUN': True
     }
-    print("START_TIMESTAMP: {}".format(STATS['START_TIMESTAMP']))
-    print("RANDOM_ID: {}".format(STATS['RANDOM_ID']))
+    print("START_TIMESTAMP: {}".format(GLOBAL['START_TIMESTAMP']))
+    print("RANDOM_ID: {}".format(GLOBAL['RANDOM_ID']))
     if len(sys.argv) > 1:
         CONFIG = read_config_from_environment(sys.argv[1])
     else:
